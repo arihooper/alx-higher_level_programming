7c7fb4241e111a7fab7db49a30eb6cec13f6192f
arihooper <arielsebsib@gmail.com>
2023-11-06 14:34:40 -0800

    task update

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,17 +1,26 @@
-#include "lists.h"
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stddef.h>
 
 /**
- * palindrom - recursive palind or not
- * @head: head list
- * Return: 0 if it is not a palindrome
- * 1 if it is a palindrome
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
  */
-int is_palindrome(listint_t **head)
+typedef struct listint_s
 {
-	if (head == NULL || *head == NULL)
-		return (1);
-	return (aux_palind(head, *head));
-}
+        int n;
+        struct listint_s *next;
+} listint_t;
+
+int aux_palind(listint_t **head, listint_t *end);
+int is_palindrome(listint_t **head);
+
+#endif /* LISTS_H */
 
 /**
  * aux_palind - funct to know if it is palindrome
@@ -20,12 +29,26 @@ int is_palindrome(listint_t **head)
  */
 int aux_palind(listint_t **head, listint_t *end)
 {
-	if (end == NULL)
-		return (1);
-	if (aux_palind(head, end->next) && (*head)->n == end->n)
-	{
-		*head = (*head)->next;
-		return (1);
-	}
-	return (0);
+        if (end == NULL)
+                return (1);
+        if (aux_palind(head, end->next) && (*head)->n == end->n)
+        {
+                *head = (*head)->next;
+                return (1);
+        }
+        return (0);
 }
+
+/**
+ * is_palindrome - recursive palind or not
+ * @head: head list
+ * Return: 0 if it is not a palindrome
+ * 1 if it is a palindrome
+ */
+int is_palindrome(listint_t **head)
+{
+        if (head == NULL || *head == NULL)
+                return (1);
+        return (aux_palind(head, *head));
+}
+
