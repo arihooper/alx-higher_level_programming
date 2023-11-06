162b8b5e856d5195c5fa432fd92f57cee4d54467
arihooper <arielsebsib@gmail.com>
2023-11-06 13:59:07 -0800

    update

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -5,4 +5,4 @@ def new_in_list(my_list, idx, element):
 
     my_list_copy = my_list.copy()
     my_list_copy[idx] = element
-    return mt_lists_copy
+    return my_list_copy
--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,6 +3,6 @@ def max_integer(my_list=[]):
     if len(my_list) < 1:
         return None
 
-    list_copy = my_list.cop()
+    list_copy = my_list.copy()
     list_copy.sort()
     return list_copy[-1]
