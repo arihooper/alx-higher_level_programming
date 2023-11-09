e25e052b00338dc2c261e7c025205bd9accbedaf
arihooper <arielsebsib@gmail.com>
2023-11-09 09:32:32 -0800

    update

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-def search_replace(my_list, search=None, replace=None):
-    return [replace if search == n else n for n in my_list
+def common_elements(set_1, set_2):
+    return set_1 & set_2
+
 
